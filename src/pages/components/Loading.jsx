6656ce44fb4bb1e9664f25b0fc3d5d0ef8da0ab4
Yash simejiya <yashsoni48678@gmail.com>
2023-03-04 00:02:11 +0530

    v1 patch done with styling and call system

--- src/pages/components/Loading.jsx
+++ src/pages/components/Loading.jsx
@@ -5,7 +5,7 @@ const Loading = () => {
     <>
       <div className="flex w-screen bg-black-opa z-50 fixed top-0 right-0 h-screen justify-center items-center ">
         <div className="flex relative w-screen h-screen justify-center items-center">
-          <div className="Load-box w-[200px] rounded-full bgs h-[200px] justify-center items-center overflow-hidden">
+          <div className="Load-box w-[125px] rounded-full bgs h-[125px] justify-center items-center overflow-hidden">
             <div className="loadGif "></div>
           </div>
         </div>
