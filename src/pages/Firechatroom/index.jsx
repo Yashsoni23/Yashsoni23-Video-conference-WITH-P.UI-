106a17593cf9a7f2327490fa4cdcc46ae7a15135
Yash simejiya <yashsoni48678@gmail.com>
2023-03-19 22:41:16 +0530

    All testing Done working fine

--- src/pages/Firechatroom/index.jsx
+++ src/pages/Firechatroom/index.jsx
@@ -17,8 +17,7 @@ const FireChatRoom = () => {
   const [data] = useCollectionData(Query);
   console.log(data);
   useEffect(() => {
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
-
+    dummy.current.scrollIntoView({ behavior: "smooth" });
   }, []);
 
   const time = Date.now();
@@ -26,33 +25,27 @@ const FireChatRoom = () => {
   const sendMsg = (e) => {
     e.preventDefault();
     firebase.sendMessage(text, time);
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
+    dummy.current.scrollIntoView({ behavior: "smooth" });
     setText("");
   };
   return (
     <>
-      <div className="flex bg-white fixed gap-20  justify-center items-end  w-screen h-screen">
+      <div className="flex  fixed gap-20  justify-center items-end  w-screen h-screen">
         <Navbar />
         <div className="hidden sm:flex w-1/2 chatbggif h-4/5 "></div>
-        <div className="flex relative chatbg sm:bg-teal-300 mbp z-20  justify-center pb-20  w-full sm:w-[305px] sm:border-[10px] border-black sm:h-[580px] h-screen rounded-3xl overflow-hidden ">
-          <span className="flex justify-start gap-3 pl-1 items-center absolute sm:w-[110px] sm:h-6 rounded-full top-2 bg-black">
+        <div className="flex relative chatbg h-screen w-screen
+         sm:bg-teal-300  z-20  justify-center pb-20  sm:w-[305px] sm:border-[10px] border-black sm:h-[580px]  sm:rounded-3xl overflow-hidden ">
+          <span className="flex justify-start gap-3 pl-1 items-center absolute sm:w-[110px] sm:h-6 sm:rounded-full top-2 bg-black">
             <span className="block rounded-full sm:w-[18px] sm:h-[18px] bg-slate-900"></span>
             <span className="block rounded-full sm:w-[62px] sm:h-[5px] bg-slate-700"></span>
             <span className="block rounded-full sm:w-[14px] sm:h-[14px] bg-slate-900"></span>
           </span>
 
-          <div className="msgs pt-10 pb-3 flex flex-col overflow-scroll absolute -z-50 w-full sm:h-[500px] h-[80%] ">
-
+          <div className="msgs  pt-20 pb-36 flex flex-col overflow-scroll absolute -z-50 w-full sm:h-[500px] h-[100%] ">
             {data && data.map((msg) => <Message key={msg.id} message={msg} />)}
-            <span ref={dummy} ></span>
+            <span ref={dummy}></span>
 
-
-
-          </div>
-
-          <div className="flex absolute p-3  justify-center items-center backdrop-blur-3xl  w-full bottom-2">
-            {/* Its Sticky Bottom Msg Keyboard */}
-            <div className="flex justify-center items-center relative w-full">
+            <div className="flex fixed justify-center items-center bottom-2 w-full">
               <textarea
                 value={text}
                 placeholder="Type your message....."
